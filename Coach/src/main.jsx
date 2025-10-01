bef6fe310c8dab61ff2464664196149d31ff2e7a
aidandawley <aidandawley62@gmail.com>
2025-10-01 16:58:42 -0700

    Seperated files. Made some mistakes on front page but Workout Planning page looks good needs fixing

--- Coach/src/main.jsx
+++ Coach/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/globals.css";
+import "./styles/components.css";
+import "./styles/planning.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
